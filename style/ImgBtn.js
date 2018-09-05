10a3aba778c06f08e9390d7314f33ec576211604
Pocus <pocus76000@gmail.com>
2018-09-05 14:18:36 +0200

    1st commit

---
+++ style/ImgBtn.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Image, TouchableOpacity, Text} from 'react-native';
+import {homeStyle} from "../style/home";
+import {fontsStyle} from "../style/fonts";
+
+export default class ProductItem extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+
+        console.log(this.props.product);
+        return (
+            <TouchableOpacity onPress={() => this.props.onPressItem(this.props.product.id)} style={homeStyle.productContainer}>
+                <Image />
+                <Text style={fontsStyle.productScanDate}>
+                    {this.props.product.date.toDateString()}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+}
